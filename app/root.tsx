929636b79dc0ccf067c1da01e530853339151af1
valkervieira <valker.vieira@gmail.com>
2022-02-21 22:25:38 -0300

    add modern css reset

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
@@ -12,6 +13,21 @@ import { getCssText } from "~/stiches.config";
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
+
+export const links: LinksFunction = () => [
+  /**
+   * CSS Reset
+   * @see https://github.com/hankchizljaw/modern-css-reset
+   */
+  {
+    rel: "preconnect",
+    href: "https://cdn.jsdelivr.net",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/modern-css-reset/dist/reset.min.css",
+  },
+];
 
 export default function App() {
   return (
